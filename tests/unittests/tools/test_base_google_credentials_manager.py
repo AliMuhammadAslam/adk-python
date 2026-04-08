545b380de19d1faeb74774350d98b9e34da4359a
George Weale <gweale@google.com>
2026-04-08 10:08:31 -0700

    added fixes for google tool and base cred
    Change-Id: I777f76b2709abfa383b508caaae9b677ed090273

--- tests/unittests/tools/test_base_google_credentials_manager.py
+++ tests/unittests/tools/test_base_google_credentials_manager.py
@@ -22,8 +22,8 @@ from google.adk.auth.auth_tool import AuthConfig
 from google.adk.tools import _google_credentials
 from google.adk.tools._google_credentials import BaseGoogleCredentialsConfig
 from google.adk.tools._google_credentials import GoogleCredentialsManager
-from google.adk.tools.bigquery.bigquery_credentials import BIGQUERY_TOKEN_CACHE_KEY
-from google.adk.tools.bigquery.bigquery_credentials import BigQueryCredentialsConfig
+from google.adk.integrations.bigquery.bigquery_credentials import BIGQUERY_TOKEN_CACHE_KEY
+from google.adk.integrations.bigquery.bigquery_credentials import BigQueryCredentialsConfig
 from google.adk.tools.tool_context import ToolContext
 from google.auth.credentials import Credentials as AuthCredentials
 from google.auth.exceptions import RefreshError
